20764476a3a8ef28ca3f1b9a60f352783a96de6e
NurProg2310 <153360973+NurProg2310@users.noreply.github.com>
2026-01-19 12:52:54 +0500

    addin css js

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	r.Static("/static", "../../static")
+	r.LoadHTMLGlob("../../templates/*")
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/auth", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "auth.html", nil)
 	})
 	r.Run(":8080")
 
